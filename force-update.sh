2c9f668e6dba29ecf22bed2fd246be8ce94f194a
Isaac Parker <parrotmac@gmail.com>
2017-04-12 01:32:58 -0600

    Fixing use of variables

--- force-update.sh
+++ force-update.sh
@@ -2,14 +2,14 @@
 
 cd /opt/heylex.us-agent/
 
-INITIAL_UPDATER_HASH = $(shasum force-update.sh)
-INITIAL_SUPERVISOR_HASH = $(shasum heylex-agent.conf)
+INITIAL_UPDATER_HASH=$(shasum force-update.sh)
+INITIAL_SUPERVISOR_HASH=$(shasum heylex-agent.conf)
 
 git fetch --all
 git reset --hard origin/master
 
-UPDATED_UPDATER_HASH = $(shasum force-update.sh)
-UPDATED_SUPERVISOR_HASH = $(shasum heylex-agent.conf)
+UPDATED_UPDATER_HASH=$(shasum force-update.sh)
+UPDATED_SUPERVISOR_HASH=$(shasum heylex-agent.conf)
 
 if [$INITIAL_UPDATER_HASH != $UPDATED_UPDATER_HASH]; then
     setsid force-update.sh &
