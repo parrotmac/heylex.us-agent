41f812a7995a1e9ad32d623a995a418fe64a712a
Isaac Parker <parrotmac@gmail.com>
2017-04-12 01:26:53 -0600

    Making updater agent smarter, maybe

--- force-update.sh
+++ force-update.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 cd /opt/heylex.us-agent/
+
+INITIAL_UPDATER_HASH = $(shasum force-update.sh)
+INITIAL_SUPERVISOR_HASH = $(shasum heylex-agent.conf)
+
 git fetch --all
 git reset --hard origin/master
+
+UPDATED_UPDATER_HASH = $(shasum force-update.sh)
+UPDATED_SUPERVISOR_HASH = $(shasum heylex-agent.conf)
+
+if [$INITIAL_UPDATER_HASH != $UPDATED_UPDATER_HASH]; then
+    setsid force-update.sh &
+    exit
+fi
+
+if [$INITIAL_SUPERVISOR_HASH != $UPDATED_SUPERVISOR_HASH]; then
+    sudo cp heylex-agent.conf /etc/supervisor/conf.d/heylex-agent.conf
+    sudo supervisorctl reload heylex-agent
+fi
+
+npm install
 sudo supervisorctl restart heylex-agent
