48c0ac61a58939107276b77382baf74d550d887c
Isaac Parker <parrotmac@gmail.com>
2017-04-12 01:53:21 -0600

    Only restarting if code changed

--- force-update.sh
+++ force-update.sh
@@ -4,13 +4,15 @@ SCRIPT_PATH=$(readlink -f "$0")
 
 cd /opt/heylex.us-agent/
 
-
+INITIAL_REPO_HASH=$(git rev-parse HEAD)
 INITIAL_UPDATER_HASH=$(shasum force-update.sh)
 INITIAL_SUPERVISOR_HASH=$(shasum heylex-agent.conf)
 
 git fetch --all
 git reset --hard origin/master
 
+
+UPDATED_REPO_HASH=$(git rev-parse HEAD)
 UPDATED_UPDATER_HASH=$(shasum force-update.sh)
 UPDATED_SUPERVISOR_HASH=$(shasum heylex-agent.conf)
 
@@ -23,7 +25,8 @@ if [ "$INITIAL_SUPERVISOR_HASH" != "$UPDATED_SUPERVISOR_HASH" ]; then
     sudo supervisorctl reload heylex-agent
 fi
 
-echo "Better? Mabe.."
-
-npm install
-sudo supervisorctl restart heylex-agent
+if [ "$INITIAL_REPO_HASH" != "$UPDATED_REPO_HASH" ]; then
+    # Only reload if things have changed
+    npm install
+    sudo supervisorctl restart heylex-agent
+fi
