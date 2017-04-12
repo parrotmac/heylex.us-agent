36145532cca770831df601d2e2084678bf867cc7
Isaac Parker <parrotmac@gmail.com>
2017-04-12 01:37:10 -0600

    Possibly actually fixing hash comparison

--- force-update.sh
+++ force-update.sh
@@ -11,12 +11,12 @@ git reset --hard origin/master
 UPDATED_UPDATER_HASH=$(shasum force-update.sh)
 UPDATED_SUPERVISOR_HASH=$(shasum heylex-agent.conf)
 
-if ["$INITIAL_UPDATER_HASH" != "$UPDATED_UPDATER_HASH"]; then
+if [ "$INITIAL_UPDATER_HASH" != "$UPDATED_UPDATER_HASH" ]; then
     setsid force-update.sh &
     exit
 fi
 
-if ["$INITIAL_SUPERVISOR_HASH" != "$UPDATED_SUPERVISOR_HASH"]; then
+if [ "$INITIAL_SUPERVISOR_HASH" != "$UPDATED_SUPERVISOR_HASH" ]; then
     sudo cp heylex-agent.conf /etc/supervisor/conf.d/heylex-agent.conf
     sudo supervisorctl reload heylex-agent
 fi
