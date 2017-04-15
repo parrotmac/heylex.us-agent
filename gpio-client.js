126550e14e249286bf0bbd2594b4754869b20887
Isaac Parker <parrotmac@gmail.com>
2017-04-14 23:07:12 -0600

    Reformatting, adding trunk pop

--- gpio-client.js
+++ gpio-client.js
@@ -9,16 +9,19 @@ var SERVICE_MODE = false;
 const PIN_REMOTE_START = 12;
 const PIN_UNLOCK = 16;
 const PIN_LOCK = 18;
+const PIN_TRUNK = 22;
 
 rpio.open(PIN_REMOTE_START, rpio.OUTPUT); // RS Trigger
-rpio.open(PIN_UNLOCK, rpio.OUTPUT); // Unlock rpio.open(PIN_LOCK, rpio.OUTPUT); // Lock
+rpio.open(PIN_UNLOCK, rpio.OUTPUT); // Unlock
+rpio.open(PIN_LOCK, rpio.OUTPUT); // Lock
+rpio.open(PIN_TRUNK, rpio.OUTPUT); // Trunk
 
-socket.on('connect', function(){
+socket.on('connect', function () {
 	console.log("Connected");
 });
 
 
-socket.on('disconnect', function(){
+socket.on('disconnect', function () {
 	console.log("Disconnected");
 });
 
@@ -38,7 +41,7 @@ function returnError(requestedAction, reason) {
 
 function pulsePin(pin, duration) {
 	var pulseDuration = 500;
-	if(typeof(duration) !== 'undefined') {
+	if (typeof (duration) !== 'undefined') {
 		pulseDuration = duration;
 	}
 	rpio.write(pin, rpio.HIGH);
@@ -49,35 +52,44 @@ function pulsePin(pin, duration) {
 var commandHistory = {};
 
 socket.on('lex-command', function incoming(actionMessage) {
-    console.log('GPIO Command: %s', actionMessage);
+	console.log('GPIO Command: %s', actionMessage);
 
-	commandHistory[actionMessage] ? commandHistory[actionMessage]+=1 : commandHistory[actionMessage] = 1;
+	commandHistory[actionMessage] ? commandHistory[actionMessage] += 1 : commandHistory[actionMessage] = 1;
 
-    if(actionMessage === "remote-start:engine:on" || actionMessage === "remote-start:engine:off") {
-		if(SERVICE_MODE) {
+	if (actionMessage === "remote-start:engine:on" || actionMessage === "remote-start:engine:off") {
+		if (SERVICE_MODE) {
 			returnError(actionMessage, 'service-mode-enabled')
 			return
 		}
-        pulsePin(PIN_REMOTE_START);
-		confirmAction(actionMessage, commandHistory[actionMessage]);
-    }
-	
-    if(actionMessage === "remote-start:security:unlock") {
-		if(SERVICE_MODE) {
-			returnError(actionMessage, 'service-mode-enabled')
-			return
-		}
-        pulsePin(PIN_UNLOCK);
+		pulsePin(PIN_REMOTE_START);
 		confirmAction(actionMessage, commandHistory[actionMessage]);
 	}
-	
-    if(actionMessage === "remote-start:security:lock") {
-		if(SERVICE_MODE) {
+
+	if (actionMessage === "remote-start:security:unlock") {
+		if (SERVICE_MODE) {
 			returnError(actionMessage, 'service-mode-enabled')
 			return
 		}
-        pulsePin(PIN_LOCK);
+		pulsePin(PIN_UNLOCK);
 		confirmAction(actionMessage, commandHistory[actionMessage]);
 	}
-	
+
+	if (actionMessage === "remote-start:security:lock") {
+		if (SERVICE_MODE) {
+			returnError(actionMessage, 'service-mode-enabled')
+			return
+		}
+		pulsePin(PIN_LOCK);
+		confirmAction(actionMessage, commandHistory[actionMessage]);
+	}
+
+	if (actionMessage === "remote-start:security:trunk") {
+		if (SERVICE_MODE) {
+			returnError(actionMessage, 'service-mode-enabled')
+			return
+		}
+		pulsePin(PIN_TRUNK);
+		confirmAction(actionMessage, commandHistory[actionMessage]);
+	}
+
 });
